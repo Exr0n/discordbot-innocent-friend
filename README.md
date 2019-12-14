7fe3e5bf34260f5cd8fbe12a1fe3b0b4affffcfb
Exr0n <25516241+Exr0n@users.noreply.github.com>
2019-12-13 23:34:05 -0800

    last commit, hopefully

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Innocent Friend
 **A memey discord bot for my friend's server**
 
-Invite with [][https://discordapp.com/api/oauth2/authorize?client_id=655305362089574450&permissions=8&scope=bot]!
+Invite with <https://discordapp.com/api/oauth2/authorize?client_id=655305362089574450&permissions=8&scope=bot>!
--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const paths = {anime_gorl: './resources/anime_gorl.jpg', shrekk: './resources/shrekk.gif'};
+
 client.on('ready', () => {
 	console.log(`Bot logged in as ${client.user.tag} at ${Date()}!`);
-	client.user.setActivity(`Playin w/ some cat6e`).catch(console.error);
+	client.user.setActivity(`w/ some cat6e`).catch(console.error);
 });
 
 client.on('message', (msg) => {
 	if (msg.author.bot) return;
 	
 	if (msg.content.trim().toLowerCase().includes('epic gamer')) {
-		if (Math.random()*1000|0 < 1) {
+		if (Math.random()*1000 < 1) {
 			msg.channel.send('UwU', { files: [paths.anime_gorl] }).catch(console.error);
 		} else {
 			msg.channel.send('', { files: [paths.shrekk] }).catch(console.error);
